5cb62c69284424199dfaba00390b5197923bbdb1
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-28 15:04:59 +0700

    refactor: (Pasien) fix some bug pasien.js

--- app_api/controller/pasien.js
+++ app_api/controller/pasien.js
@@ -3,99 +3,79 @@ const mongoose = require("mongoose");
 const pasien = mongoose.model("Pasien");
 
 const pasienList = (req, res) => {
-    pasien.find({},
-        function (err, result){
-            if(err){
-                res.status(500)
-                .json({
-                    "status": "failed"
-                });
-            } else {
-                res.status(200)
-                .json(result);
-            }
-        });
-}
+  pasien.find({}, function (err, result) {
+    if (err) {
+      res.status(500).json({
+        status: "failed",
+      });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+};
 const pasienCreate = (req, res) => {
-    pasien.create({
-        nama: req.body.nama,
-        tgl_lahir: req.body.tgl_lahir,
-        tempat_lahir: req.body.tmpt_lahir,
-        jenis_kelamin: req.body.jenis_kelamin,
-        umur: req.body.umur,
-        gol_darah: req.body.gol_darah,
-        tinggi_badan: req.body.tinggi_badan,
-        berat_badan: req.body.berat_badan,
-        alamat: req.body.alamat,
-        no_tlpn: req.body.no_tlpn
-    }, (err, result) => {
-        if (err) {
-            res
-                .status(400)
-                .json(err);
-        } else {
-            res
-                .status(201)
-                .json(result);
-        }
-    });
-}
+  pasien.create(
+    {
+      nama: req.body.nama,
+      tgl_lahir: req.body.tgl_lahir,
+      tempat_lahir: req.body.tempat_lahir,
+      jenis_kelamin: req.body.jenis_kelamin,
+      umur: req.body.umur,
+      gol_darah: req.body.gol_darah,
+      tinggi_badan: req.body.tinggi_badan,
+      berat_badan: req.body.berat_badan,
+      alamat: req.body.alamat,
+      no_tlpn: req.body.no_tlpn,
+    },
+    (err, result) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(201).json(result);
+      }
+    }
+  );
+};
 const pasienReadOne = (req, res) => {
-    pasien
-        .findById(req.params.id)
-        .exec((err,pasien) => {
-            res
-                .status(200)
-                .json(psn);
-        });
-}
+  pasien.findById(req.params.id).exec((err, pasien) => {
+    res.status(200).json(pasien);
+  });
+};
 const pasienUpdateOne = (req, res) => {
-    pasien
-        .findById(req.params.id)
-        .exec((err, pasien) => {
-            pasien.nama = req.body.nama,
-            pasien.tgl_lahir = req.body.tgl_lahir,
-            pasien.tempat_lahir = req.body.tmpt_lahir,
-            pasien.jenis_kelamin = req.body.jenis_kelamin,
-            pasien.umur = req.body.umur,
-            pasien.gol_darah = req.body.gol_darah,
-            pasien.tinggi_badan = req.body.tinggi_badan,
-            pasien.berat_badan = req.body.berat_badan,
-            pasien.alamat = req.body.alamat,
-            pasien.no_tlpn = req.body.no_tlpn
-            pasien.save((err, result) => {
-                if (err) {
-                    res
-                        .status(404)
-                        .json(result);
-                } else {
-                    res
-                        .status(200)
-                        .json(result);
-                }
-            });
-        })
-}
+  pasien.findById(req.params.id).exec((err, pasien) => {
+    (pasien.nama = req.body.nama),
+      (pasien.tgl_lahir = req.body.tgl_lahir),
+      (pasien.tempat_lahir = req.body.tmpt_lahir),
+      (pasien.jenis_kelamin = req.body.jenis_kelamin),
+      (pasien.umur = req.body.umur),
+      (pasien.gol_darah = req.body.gol_darah),
+      (pasien.tinggi_badan = req.body.tinggi_badan),
+      (pasien.berat_badan = req.body.berat_badan),
+      (pasien.alamat = req.body.alamat),
+      (pasien.no_tlpn = req.body.no_tlpn);
+    pasien.save((err, result) => {
+      if (err) {
+        res.status(404).json(result);
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  });
+};
 const pasienDeleteOne = (req, res) => {
-    pasien
-        .findById(req.params.id)
-        .exec((err, pasien) => {
-            pasien.remove((err, result) => {
-                if (err) {
-                    res
-                        .satatus(404)
-                        .json(err);
-                }
-                res
-                    .status(204)
-                    .json(null);
-            });
-        });
-}
+  pasien.findById(req.params.id).exec((err, pasien) => {
+    pasien.remove((err, result) => {
+      if (err) {
+        res.satatus(404).json(err);
+      }
+      res.status(204).json(null);
+    });
+  });
+};
 module.exports = {
-    pasienCreate,
-    pasienDeleteOne,
-    pasienList,
-    pasienReadOne,
-    pasienUpdateOne
-}
+  pasienCreate,
+  pasienDeleteOne,
+  pasienList,
+  pasienReadOne,
+  pasienUpdateOne,
+};
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,21 +1,22 @@
-let mongoose = require("mongoose")
+let mongoose = require("mongoose");
 
-let dbURI = "mongodb+srv://si:sisteminformasi@cluster0.sscpnlc.mongodb.net/si51?retryWrites=true&w=majority";
+// let dbURI = "mongodb+srv://si:sisteminformasi@cluster0.sscpnlc.mongodb.net/si51?retryWrites=true&w=majority";
+let dbURI = "mongodb://127.0.0.1:27017/si51";
 
 mongoose.connect(dbURI, {
-    useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
 mongoose.connection.on("connected", () => {
-    console.log("Connected to MongoDB");
+  console.log("Connected to MongoDB");
 });
 
 mongoose.connection.on("error", (error) => {
-    console.log("Connection Error : " + error);
+  console.log("Connection Error : " + error);
 });
 
 mongoose.connection.on("disconnected", () => {
-    console.log("Disconnected from MongoDB");
+  console.log("Disconnected from MongoDB");
 });
 //memanggil model
 require("./pasien");
